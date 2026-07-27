1b84a92bd5768d9abcdb1d641be815e9500ca000
Ryan Lima <ryan.limaa@outlook.com>
2026-07-27 12:04:15 -0300

    Project created

---
+++ SistemaVendedoresWebMcv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaVendedoresWebMcv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
