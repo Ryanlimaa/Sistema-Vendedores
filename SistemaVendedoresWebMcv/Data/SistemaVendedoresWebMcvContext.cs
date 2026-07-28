141d5a02e0b21d3318a4885068e230476ede476c
Ryan Lima <ryan.limaa@outlook.com>
2026-07-28 09:02:55 -0300

    Gerando CRUD Scaffolding para Departamento

---
+++ SistemaVendedoresWebMcv/Data/SistemaVendedoresWebMcvContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaVendedoresWebMcv.Models;
+
+namespace SistemaVendedoresWebMcv.Data
+{
+    public class SistemaVendedoresWebMcvContext : DbContext
+    {
+        public SistemaVendedoresWebMcvContext (DbContextOptions<SistemaVendedoresWebMcvContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaVendedoresWebMcv.Models.Departamento> Departamento { get; set; } = default!;
+    }
+}
--- SistemaVendedoresWebMcv/Program.cs
+++ SistemaVendedoresWebMcv/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVendedoresWebMcv.Data;
 namespace SistemaVendedoresWebMcv
 {
     public class Program
@@ -5,6 +8,8 @@ namespace SistemaVendedoresWebMcv
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<SistemaVendedoresWebMcvContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SistemaVendedoresWebMcvContext") ?? throw new InvalidOperationException("Connection string 'SistemaVendedoresWebMcvContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
