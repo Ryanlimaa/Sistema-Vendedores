ab996c1a29e7c97f2ca49d2133c35790a464d8a2
Ryan Lima <ryan.limaa@outlook.com>
2026-07-30 13:33:02 -0300

    Criação de Controller e View de Vendedores

---
+++ SistemaVendedoresWebMcv/Controllers/VendedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVendedoresWebMcv.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
